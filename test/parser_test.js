bfaeb37f14f8f43799f3367c5bf04fae53d59eda
Francis Chong <francis@ignition.hk>
2014-12-20 19:43:37 +0800

    add some other tag_change

--- test/parser_test.js
+++ test/parser_test.js
@@ -35,5 +35,36 @@ describe('Parser', function(){
         expect(result).to.deep.equal(["mode", "solo"]);
     });
 
+    describe("TAG_CHANGE", function(){
+      it("should find player id", function(){
+        result = parser.parseLine("[Power] GameState.DebugPrintPower() - TAG_CHANGE Entity=siuying tag=PLAYER_ID value=2")
+        expect(result).to.deep.equal(["player_id", {name: "siuying", player_id: 2}]);
+      })
+
+      it("should find first player", function(){
+        result = parser.parseLine("[Power] GameState.DebugPrintPower() - TAG_CHANGE Entity=begize tag=FIRST_PLAYER value=1")
+        expect(result).to.deep.equal(["first_player", {name: "begize"}]);
+      })
+
+      it("should find player win state", function(){
+        result = parser.parseLine("[Power] GameState.DebugPrintPower() - TAG_CHANGE Entity=begize tag=PLAYSTATE value=WON")
+        expect(result).to.deep.equal(["game_over", {name: "begize", state: "WON"}]);
+      })
+
+      it("should find game start", function(){
+        result = parser.parseLine("[Power] GameState.DebugPrintPower() - TAG_CHANGE Entity=GameEntity tag=TURN_START value=1418310864")
+        expect(result).to.deep.equal(["game_start", {timestamp: 1418310864}]);
+      })
+
+      it("should find turn start", function(){
+        result = parser.parseLine("[Power] GameState.DebugPrintPower() -     TAG_CHANGE Entity=siuying tag=TURN_START value=1418309639")
+        expect(result).to.deep.equal(["turn_start", {name: "siuying", timestamp: 1418309639}]);
+      })
+
+      it("should find turn number", function(){
+        result = parser.parseLine("[Power] GameState.DebugPrintPower() -     TAG_CHANGE Entity=GameEntity tag=TURN value=3")
+        expect(result).to.deep.equal(["turn", 3]);
+      })
+    });
   });
 })
