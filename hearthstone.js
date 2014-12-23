2e123dbefa46eda539d0624cd9dda0bcd9ec14be
Francis Chong <francis@ignition.hk>
2014-12-23 19:28:43 +0800

    make sure traceur is loaded in runtime

--- hearthstone.js
+++ hearthstone.js
@@ -1,3 +1,7 @@
+// load traceur runtime
+require('traceur/bin/traceur-runtime');
+
+// load modules
 var Hearthstone = {
     Logger: require('./build/logger'),
     Player: require('./build/player'),
