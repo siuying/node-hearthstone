51d10431ac0baa61046992490c72718105fc8022
Francis Chong <francis@ignition.hk>
2014-12-25 03:00:10 +0800

    remove console log, replace them with limited message in cli

--- build/logger.js
+++ build/logger.js
@@ -11,28 +11,20 @@ $traceurRuntime.ModuleStore.getAnonymousModule(function() {
     this.outputPath = outputPath;
     this.game = new Game(null);
     var logger = this;
-    this.parser.on('startup', function(mode) {
-      console.log('startup');
-    });
     this.parser.on('mode', function(mode) {
-      console.log(("Mode: " + mode));
       logger.mode = mode;
       logger.game.mode = mode;
     });
     this.parser.on('action_start', function(data) {
-      console.log("action_start:", data);
       logger.game.addEvent(['action_start', data]);
     });
     this.parser.on('zone_change', function(data) {
-      console.log("zone_change:", data);
       logger.game.addEvent(['zone_change', data]);
     });
     this.parser.on('pos_change', function(data) {
-      console.log("pos_change:", data);
       logger.game.addEvent(['pos_change', data]);
     });
     this.parser.on('tag_change', function(data) {
-      console.log("tag_change:", data);
       logger.game.addEvent(['tag_change', data]);
       var type = data.type;
       if (type === "game_over") {
